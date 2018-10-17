d97c12defb13fa39d6d50f620e4b6dd86f06df3f
dambusm <willschaller@gmail.com>
2018-10-17 17:23:54 +0200

    Add validation to post route

--- routes/build.js
+++ routes/build.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const validator = require('validator');
 
 const Build = require('../models/builds');
 
 router.post('/', (req, res, next) => {
-  if (!req.body.form) {
+  if (
+    !req.body.form ||
+    !validator.isURL(req.body.form.url) ||
+    !validator.isAlphanumeric(req.body.form.url) ||
+    !validator.isBase64(req.body.form.splashScreen) ||
+    !validator.isBase64(req.body.form.launcherIcon) ||
+    !validator.isHexColor(req.body.form.primaryColor) ||
+    !validator.isHexColor(req.body.form.secondaryColor) ||
+    !validator.isBoolean(req.body.form.camera) ||
+    !validator.isBoolean(req.body.form.externalUrls) ||
+    !validator.isBoolean(req.body.form.gps) ||
+    !validator.isBoolean(req.body.form.landscape) ||
+    !validator.isBoolean(req.body.form.portrait) ||
+    !validator.isBoolean(req.body.form.progressBar) ||
+    !validator.isBoolean(req.body.form.ratingDays) ||
+    !validator.isBoolean(req.body.form.uploads) ||
+    !validator.isBoolean(req.body.form.zoom) ||
+    !validator.isInt(req.body.form.ratingDays)
+  ) {
     res.status(422).json({ code: 'incorrect parameters' });
   }
-
-  res.send('hello world');
+  const data = req.body.form;
+  const build = new Build(data);
+  build
+    .save()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(next);
 });
 
 module.exports = router;
