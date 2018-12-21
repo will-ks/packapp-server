f31e94d5c23bf12d5fdf9145ee44ca8421331e23
dambusm <willschaller@gmail.com>
2018-12-21 18:55:59 +0100

    Prettify

--- models/builds.js
+++ models/builds.js
@@ -1,102 +1,104 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const buildSchema = new Schema({
-  appName: {
-    type: String,
-    required: true,
-    maxlength: 50
+const buildSchema = new Schema(
+  {
+    appName: {
+      type: String,
+      required: true,
+      maxlength: 50
+    },
+    url: {
+      type: String,
+      required: true,
+      maxlength: 2083
+    },
+    splashScreen: {
+      type: String,
+      required: true,
+      maxlength: 30
+    },
+    launcherIcon: {
+      type: String,
+      required: true,
+      maxlength: 30
+    },
+    primaryColor: {
+      type: String,
+      required: true,
+      maxlength: 9
+    },
+    secondaryColor: {
+      type: String,
+      required: true,
+      maxlength: 9
+    },
+    camera: {
+      type: Boolean,
+      required: true
+    },
+    externalUrls: {
+      type: Boolean,
+      required: true
+    },
+    gps: {
+      type: Boolean,
+      required: true
+    },
+    landscape: {
+      type: Boolean,
+      required: true
+    },
+    portrait: {
+      type: Boolean,
+      required: true
+    },
+    progressBar: {
+      type: Boolean,
+      required: true
+    },
+    ratingDays: {
+      type: Number,
+      required: true,
+      default: 3,
+      max: 2000
+    },
+    ratings: {
+      type: Boolean,
+      required: true
+    },
+    uploads: {
+      type: Boolean,
+      required: true
+    },
+    zoom: {
+      type: Boolean,
+      required: true
+    },
+    orientation: {
+      type: String,
+      enum: ['portrait', 'landscape', 'unspecified'],
+      required: true
+    },
+    builtApk: {
+      type: String
+    },
+    sourceZip: {
+      type: String
+    },
+    buildError: {
+      type: String
+    },
+    building: {
+      type: Boolean,
+      default: false
+    },
+    downloadUrl: String,
+    sourceDownloadUrl: String,
+    userEmail: String
   },
-  url: {
-    type: String,
-    required: true,
-    maxlength: 2083
-  },
-  splashScreen: {
-    type: String,
-    required: true,
-    maxlength: 30
-  },
-  launcherIcon: {
-    type: String,
-    required: true,
-    maxlength: 30
-  },
-  primaryColor: {
-    type: String,
-    required: true,
-    maxlength: 9
-  },
-  secondaryColor: {
-    type: String,
-    required: true,
-    maxlength: 9
-  },
-  camera: {
-    type: Boolean,
-    required: true
-  },
-  externalUrls: {
-    type: Boolean,
-    required: true
-  },
-  gps: {
-    type: Boolean,
-    required: true
-  },
-  landscape: {
-    type: Boolean,
-    required: true
-  },
-  portrait: {
-    type: Boolean,
-    required: true
-  },
-  progressBar: {
-    type: Boolean,
-    required: true
-  },
-  ratingDays: {
-    type: Number,
-    required: true,
-    default: 3,
-    max: 2000
-  },
-  ratings: {
-    type: Boolean,
-    required: true
-  },
-  uploads: {
-    type: Boolean,
-    required: true
-  },
-  zoom: {
-    type: Boolean,
-    required: true
-  },
-  orientation: {
-    type: String,
-    enum: ['portrait', 'landscape', 'unspecified'],
-    required: true
-  },
-  builtApk: {
-    type: String
-  },
-  sourceZip: {
-    type: String
-  },
-  buildError: {
-    type: String
-  },
-  building: {
-    type: Boolean,
-    default: false
-  },
-  downloadUrl: String,
-  sourceDownloadUrl: String,
-  userEmail: String
-},
-{ timestamps: true });
+  { timestamps: true }
+);
 
 const Item = mongoose.model('Build', buildSchema);
 
